82c3384ebf0659c708adcb68e3899f18f2b1fa3b
unknown <intern@Development.indybuild.com>
2015-08-18 11:33:37 -0500

    personal website changes

---
+++ js/index.js
@@ -0,0 +1,21 @@
+angular
+  .module('myApp', [])
+ 
+  .controller('myCtrl', function DemoCtrl() {
+    
+
+    
+
+    this.call = function() {
+      
+    
+
+    };
+
+    this.checkVoicemail = function() {
+      // This never happens.
+    };
+    this.contact = function() {
+    
+    };
+  });
