647e9a017670cf92543241c8f79027f520127cf8
unknown <intern@Development.indybuild.com>
2015-08-18 14:52:23 -0500

    personal wesbsite changes

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
-angular
-  .module('myApp', [])
- 
-  .controller('myCtrl', function DemoCtrl() {
-    
+var myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config(function ($routeProvider) {
     
-
-    this.call = function() {
-      
+    $routeProvider
     
-
-    };
-
-    this.checkVoicemail = function() {
-      // This never happens.
-    };
-    this.contact = function() {
+     
+    .when('/home',{
+        templateUrl:'pages/home.html',
+        controller:'homeCtrl'
+    })
     
-    };
-  });
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+});
+
+myApp.controller('homeCtrl', ['$scope', '$log', function($scope, $log) {
+    
+    $scope.name = 'Main';
+    
+}]);
+
+myApp.controller('myCtrl', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    
+    $scope.num = $routeParams.num || 1;
+    
+}]);
